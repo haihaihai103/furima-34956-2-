a456542536632a8aae14f79352c669ee610996c6
haihaihai103 <80326285+haihaihai103@users.noreply.github.com>
2021-05-11 17:22:39 +0900

    テストコードエラー分修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,14 @@ class User < ApplicationRecord
   has_many :items
   has_many :customers
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, #message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
     validates :first_name
     validates :last_name
   end  
 
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, #message: "は6文字以上かつ英数字をそれぞれ含めてください" }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   validates :nickname, presence: true
   validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/}
   validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/}
   validates :birth_date, presence: true
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :user do
     nickname { 'test' }
-    last_name { '太郎' }
     first_name { '山田' }
-    last_name_kana { 'タロウ' }
+    last_name { '太郎' }
     first_name_kana { 'ヤマダ' }
+    last_name_kana { 'タロウ' }
     email {Faker::Internet.free_email}
     password {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
