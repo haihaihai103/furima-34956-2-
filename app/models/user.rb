a56b902919ebb0273dc0ac41c20cb1d791ec9908
haihaihai103 <80326285+haihaihai103@users.noreply.github.com>
2021-05-17 12:52:13 +0900

    テストコード修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,17 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-    validates :last_name_kana, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/}
-    validates :first_name_kana, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/}
     validates :birth_date
-  end
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+
+    with_options format:  { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+      validates :first_name
+      validates :last_name
+    end
+
+    with_options format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/}
+      validates :last_name_kana 
+      validates :first_name_kana
+    end
+   end
 end
