f33bd49dc32a849f1e7ddac0467b0e4113afb201
haihaihai103 <80326285+haihaihai103@users.noreply.github.com>
2021-05-25 14:17:30 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,9 +47,8 @@ end
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     unless current_user.id == @item.user_id
-      redirect_to new_user_session_path
+      redirect_to root_path
     end
   end
 
